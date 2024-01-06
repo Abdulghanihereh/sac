ed58fc61c05d06470b6ac995a67f7f98f1e91dff
Abdulghanihereh <abdulghaniwebdesign1@gmail.com>
2024-01-06 20:35:48 +0300

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+for (var f=113 ; f<=250 ; f+=11 ) {
+    if(f % 2 == 0){
+        document.write(`<h1 style='color:red'>  ${f} </h1>`);
+    }
+    else if(f % 5 == 0){
+        document.write(`<h1 style='color:yellow'>  ${f} </h1>`);
+    }
+    else{
+        document.write(`<h1 style='color:black'>  ${f} </h1>`);
+    }
+}
